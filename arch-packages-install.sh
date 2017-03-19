b3f6ff878cbaf249cdb2f19dcaa37be6b0cd8d5e
sevaho <sebastiaan.vanhoecke@hotmail.be>
2017-03-19 19:58:39 +0100

    added libisoburn and mtools

--- arch-packages-install.sh
+++ arch-packages-install.sh
@@ -29,7 +29,7 @@ packages(){
   #Compression
   pacman -S --noconfirm zip unzip unrar unace 
   #Other
-  pacman -S --noconfirm dkms exfat-utils synaptics
+  pacman -S --noconfirm dkms exfat-utils synaptics mtools libisoburn
   #Fonts, themes & icons
   pacman -S --noconfirm shadow arc-icon-theme arc-solid-gtk-theme ttf-font-awesome 
 
